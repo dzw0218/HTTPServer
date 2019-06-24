396ac7debb54147a2a8f9fe1bbb6c1c1cb37ac04
dzw <dzw0218@163.com>
2019-06-24 19:23:01 +0800

    完成Socket接口类

---
+++ include/mainserver.h
@@ -0,0 +1,16 @@
+#ifndef MAINSERVER_H_
+#define MAINSERVER_H_
+
+#include "socketinterface.h"
+
+namespace Socket
+{
+
+class MainServer : public socketinterface
+{
+    
+};
+
+}
+
+#endif // MAINSERVER_H_
---
+++ include/socketinterface.h
@@ -0,0 +1,55 @@
+/**
+* socket接口类
+ */
+
+#ifndef SOCKETINTERFACE_H_
+#define SOCKETINTERFACE_H_
+
+#include <iostream>
+#include <string>
+
+namespace SocketInterface
+{
+
+//HTTP连接客户端接口
+class ClientInterface
+{
+public:
+    virtual ~ClientInterface()
+    {
+
+    }
+
+    virtual int fd() = 0;
+    //初始化套接字，0/成功，-1/失败
+    virtual int initSocket() = 0;
+    //设置该套接字是否阻塞，0/成功，-1/失败
+    virtual int setNonBlocking(bool block) = 0;
+    //关闭套接字，0/成功，1/失败
+    virtual int closeSocket() = 0;
+    //virtual int connect(const std::string &ip, unsigned int port) = 0;
+    virtual size_t recv(void *buf, size_t len, int flags) = 0;
+    virtual size_t send(const void *buf, size_t len, int flags) = 0;
+};
+
+//HTTP服务器接口
+class ServerInterface
+{
+public:
+    virtual ~ServerInterface()
+    {
+
+    }
+
+    virtual int fd() = 0;
+    virtual int initSocket(size_t backlog) = 0;
+    //设置该套接字是否阻塞，0/成功，-1/失败
+    virtual int setNonBlocking(bool block) = 0;
+    virtual int closeSocket() = 0;
+    virtual bool isRun() = 0;
+    virtual ClientInterface *acceptSocket() = 0;
+};
+
+}
+
+ #endif // SOCKETINTERFACE_H_

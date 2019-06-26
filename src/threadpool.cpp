0df837244440b61d0c63ad079b9ad20c72461eaf
dzw <dzw0218@163.com>
2019-06-26 19:30:07 +0800

    初始化threadpool类

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -7,7 +7,7 @@ MyThreadPool::MyThreadPool(size_t threadnum, size_t tasknum)
     : m_threadnum(threadnum), 
     m_hasleader(false)
 {
-    m_threadnum = ()
+    m_threadnum = ();
 }
 
 } // namespace ThreadPool

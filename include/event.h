13401bba8adfb1586cee1ffc19953101166710e4
dzw <dzw0218@163.com>
2019-06-27 22:21:51 +0800

    完成事件类

--- include/event.h
+++ include/event.h
@@ -6,9 +6,12 @@ epoll事件接口
 #define EVENT_H_
 
 #include "socketinterface.h"
+#include "threadpool.h"
 #include <iostream>
 #include <sys/epoll.h>
 #include <mutex>
+#include <map>
+
 
 namespace Event
 {
@@ -28,9 +31,11 @@ enum EventType
 
 class Observer //虚基类
 {
+    friend class MyObserver;
 public:
     virtual ~Observer(){};
 
+protected:
     virtual void epoll_handlein(int) = 0;
     virtual void epoll_handleout(int) = 0;
     virtual void epoll_handleclose(int) = 0;
@@ -48,14 +53,15 @@ public:
 
 class MyObserver : public Observer //主要观察者
 {
+    friend class MyEvent;
 public:
     MyObserver(Observer &observer, EventType);
     ~MyObserver();
 
     inline int increaseRef();
     inline int reduceRef();
-    inline int countRef();
-    inline bool release();
+    inline bool countRef();
+    inline void release();
     inline EventType getEventType();
     inline const Observer *getHandle();
 
@@ -64,7 +70,7 @@ protected:
     void epoll_handlein(int);
     void epoll_handleout(int);
     void epoll_handleclose(int);
-    void epoll_handleerror(int);
+    void epoll_handleerr(int);
 
 private:
     std::mutex m_mutex;
@@ -80,16 +86,69 @@ class Event //事件虚基类
 public:
     ~Event(){};
 
-    virtual int register_event(int, ThreadHandle*, EventType) = 0;
+    virtual int register_event(int, Handler *, EventType) = 0;
     virtual int shutdown_event(int) = 0;
 };
 
-class MyEvent : public Event // 主事件类
+class MyEvent : public Event, public ThreadPool::ThreadHandle // 主事件类
 {
 public:
     MyEvent(size_t event_max);
     ~MyEvent();
-    
+
+    int register_event(int, Handler *, EventType);
+    int shutdown_event(int);
+
+protected:
+    void taskHandle();
+
+private:
+    enum Exist{
+        NotExist, Modify, HandleModify, TypeModify, Existed
+    };
+    enum Config{
+        EventLen = 1024,
+    };
+    typedef std::map<int, MyObserver*> EventObserver_t;
+    typedef std::map<int, EventType> EventType_t;
+
+    Exist isExist(int fd, EventType type, Handler *handler);
+    int record(int fd, EventType type, Handler *handler);
+    int detach(int fd, bool release = false);
+    MyObserver *getObserver(int fd);
+    int pushTask(int fd, EventType type);
+    int popTask(int &fd, EventType &type);
+    size_t taskSize();
+    int cleartask(int fd);
+    int unregister_event(int);
+    static void *eventwait(void *args);
+
+private:
+    int m_epollfd;
+    EventObserver_t m_eventobserver;
+    std::mutex m_eventobserver_mutex;
+    EventType_t m_eventtype;
+    std::mutex m_eventtype_mutex;
+
+    struct epoll_event m_event[EventLen];
+    ThreadPool::ThreadPool *m_threadpool;
+    std::thread m_thread;
+};
+
+class EventProxy : public Event //事件类代理
+{
+public:
+    static EventProxy *instance();
+    int register_event(int, Handler *, EventType);
+    int register_event(SocketInterface::SocketDest &, Handler*, EventType);
+    int shutdown_event(int);
+    int shutdown_event(SocketInterface::SocketDest &);
+
+private:
+    EventProxy(int event_max);
+    ~EventProxy();
+
+    Event *m_event;
 };
 
 } // namespace EVENT_H_
--- src/event.cpp
+++ src/event.cpp
@@ -1,2 +1,365 @@
 #include "event.h"
+#include "log.h"
 
+namespace Event
+{
+//handler类定义
+int Handler::register_event(int fd, EventType type)
+{
+    return EventProxy::instance()->register_event(fd, this, type);
+}
+
+int Handler::register_event(SocketInterface::SocketDest &socket, EventType type)
+{
+    return EventProxy::instance()->register_event(socket, this, type);
+}
+
+int Handler::shutdown_event(int fd)
+{
+    return EventProxy::instance()->shutdown_event(fd);
+}
+
+int shutdown_event(SocketInterface::SocketDest &socket)
+{
+    return EventProxy::instance()->shutdown_event(socket);
+}
+
+//MyObserver类的定义
+MyObserver::MyObserver(Observer &observer, EventType type)
+    : m_observer(observer),
+    m_eventtype(type),
+    m_refcount(1)
+{
+}
+
+MyObserver::~MyObserver()
+{
+}
+
+int MyObserver::increaseRef()
+{
+    std::unique_lock<std::mutex> lck(m_mutex);
+    m_refcount++;
+}
+
+int MyObserver::reduceRef()
+{
+    std::unique_lock<std::mutex> lck(m_mutex);
+    m_refcount--;
+}
+
+bool MyObserver::countRef()
+{
+    std::unique_lock<std::mutex> lck(m_mutex);
+    m_refcount--;
+    return (m_refcount == 0);
+}
+
+void MyObserver::release()
+{
+    delete this;
+}
+
+EventType MyObserver::getEventType()
+{
+    return m_eventtype;
+}
+
+const Observer* MyObserver::getHandle()
+{
+    return &m_observer;
+}
+
+void MyObserver::epoll_handlein(int fd)
+{
+    m_observer.epoll_handlein(fd);
+}
+
+void MyObserver::epoll_handleout(int fd)
+{
+    m_observer.epoll_handleout(fd);
+}
+
+void MyObserver::epoll_handleclose(int fd)
+{
+    m_observer.epoll_handleclose(fd);
+}
+
+void MyObserver::epoll_handleerr(int fd)
+{
+    m_observer.epoll_handleerr(fd);
+}
+
+//MyEvent实现
+MyEvent::MyEvent(size_t event_max)
+{
+    m_epollfd = epoll_create(event_max);
+    if(m_epollfd < 0)
+        Logger::log(Logger::All, "epoll create failed.");
+
+    m_threadpool = ThreadPool::ThreadPoolProxy::instance();
+
+    m_thread = std::thread(&MyEvent::eventwait, this);
+}
+
+MyEvent::~MyEvent()
+{
+    if(m_thread.joinable())
+        m_thread.join();
+
+    if(m_epollfd > 0)
+        close(m_epollfd);
+}
+
+int MyEvent::register_event(int fd, Handler *handle, EventType type)
+{
+    if(fd == INVALID_SOCKET || handle == nullptr || m_epollfd == -1)
+        return -1;
+
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = type;
+
+    Exist ret = isExist(fd, type, handle);
+    if(ret == Existed)
+        return 0;
+
+    record(fd, type, handle);
+    if(ret == HandleModify)
+        return 0;
+
+    int opt;
+    if(ret == TypeModify || ret == Modify)
+        opt = EPOLL_CTL_MOD;
+    else if(ret == NotExist)
+        opt = EPOLL_CTL_ADD;
+
+    if(epoll_ctl(m_epollfd, opt, fd, &event) < 0)
+    {
+        detach(fd, true);
+        return -1;
+    }
+
+    return 0;
+}
+
+int MyEvent::unregister_event(int fd)
+{
+    if(epoll_ctl(m_epollfd, EPOLL_CTL_DEL, fd, nullptr) < 0)
+        return -1;
+
+    return detach(fd);
+}
+
+int MyEvent::shutdown_event(int fd)
+{
+    return ::shutdown(fd, SHUT_WR);
+}
+
+MyEvent::Exist MyEvent::isExist(int fd, EventType type, Handler *handler)
+{
+    std::unique_lock<std::mutex> lck(m_eventobserver_mutex);
+    EventObserver_t::iterator iter = m_eventobserver.find(fd);
+    if(iter == m_eventobserver.end())
+        return NotExist;
+
+    MyObserver &observer = *(iter->second);
+    EventType oldType = observer.getEventType();
+    const Observer *oldHandler = observer.getHandle();
+    if(oldType != type && oldHandler != handler)
+        return Modify;
+    else if(oldType != type)
+        return TypeModify;
+    else if(oldHandler != handler)
+        return HandleModify;
+
+    return Existed;
+}
+
+int MyEvent::record(int fd, EventType type, Handler *handler)
+{
+    MyObserver *observer = new MyObserver(*handler, type);
+
+    std::unique_lock<std::mutex> lck(m_eventobserver_mutex);
+    m_eventobserver[fd] = observer;
+    return 0;
+}
+
+int MyEvent::detach(int fd, bool release)
+{
+    std::unique_lock<std::mutex> lck(m_eventobserver_mutex);
+    EventObserver_t::iterator iter = m_eventobserver.find(fd);
+    if(iter == m_eventobserver.end())
+        return -1;
+
+    if(release)
+        iter->second->release();
+
+    m_eventobserver.erase(iter);
+    return 0;
+}
+
+MyObserver* MyEvent::getObserver(int fd)
+{
+    std::unique_lock<std::mutex> lck(m_eventobserver_mutex);
+    EventObserver_t::iterator iter = m_eventobserver.find(fd);
+    if(iter == m_eventobserver.end())
+        return nullptr;
+
+    MyObserver *observer = m_eventobserver[fd];
+    observer->increaseRef();
+    return observer;
+}
+
+int MyEvent::pushTask(int fd, EventType type)
+{
+    std::unique_lock<std::mutex> lck(m_eventtype_mutex);
+    EventType_t::iterator iter = m_eventtype.find(fd);
+    if(iter == m_eventtype.end())
+    {
+        m_eventtype[fd] = type;
+        return 0;
+    }
+
+    iter->second = (EventType)(iter->second | type);
+    return 0;
+}
+
+int MyEvent::popTask(int &fd, EventType &type)
+{
+    std::unique_lock<std::mutex> lck(m_eventtype_mutex);
+    EventType_t::iterator iter = m_eventtype.begin();
+    if(iter == m_eventtype.end())
+        return -1;
+
+    fd = iter->first;
+    type = iter->second;
+    m_eventtype.erase(iter);
+    return 0;
+}
+
+int MyEvent::cleartask(int fd)
+{
+    std::unique_lock<std::mutex> lck(m_eventtype_mutex);
+    if(fd == -1)
+    {
+        m_eventtype.clear();
+        return 0;
+    }
+    else if(fd > 0)
+    {
+        EventType_t::iterator iter = m_eventtype.find(fd);
+        if(iter == m_eventtype.end())
+        {
+            return -1;
+        }
+        m_eventtype.erase(iter);
+        return 0;
+    }
+    return -1;
+}
+
+size_t MyEvent::taskSize()
+{
+    std::unique_lock<std::mutex> lck(m_eventtype_mutex);
+    return m_eventtype.size();
+}
+
+void MyEvent::taskHandle()
+{
+    int fd = 0;
+    EventType type;
+    if(popTask(fd, type) < 0)
+        return;
+
+    MyObserver *observer = getObserver(fd);
+    if(observer == nullptr)
+        return;
+
+    if(type & ECLOSE)
+    {
+        cleartask(fd);
+        observer->reduceRef();
+    }
+    else if(type & EERR)
+        observer->epoll_handleerr(fd);
+    else if(type & EIN)
+        observer->epoll_handlein(fd);
+    else if(type & EOUT)
+        observer->epoll_handleout(fd);
+
+    if(observer->countRef())
+    {
+        unregister_event(fd);
+        observer->epoll_handleclose(fd);
+        observer->release();
+    }
+}
+
+void* MyEvent::eventwait(void *args)
+{
+    MyEvent &event = *(MyEvent*)args;
+    if(event.m_epollfd == -1)
+        return nullptr;
+
+    while(true)
+    {
+        int nEvent = epoll_wait(event.m_epollfd, &event.m_event[0], EventLen, -1);
+        if(nEvent < 0 && errno == EINTR)
+            break;
+
+        for(int index = 0; index < nEvent; ++index)
+        {
+            int fd = event.m_event[index].data.fd;
+            EventType type = static_cast<EventType>(event.m_event[index].events);
+
+            if(event.pushTask(fd, type) == 0)
+            {
+                event.m_threadpool->pushTask(&event);
+            }
+        }
+    }
+}
+
+EventProxy* EventProxy::instance()
+{
+    static EventProxy *eventproxy = nullptr;
+    if(eventproxy == nullptr)
+    {
+        eventproxy = new EventProxy(EVENT_MAX);
+    }
+    return eventproxy;
+}
+
+EventProxy::EventProxy(int event_max)
+{
+    m_event = new MyEvent(event_max);
+}
+
+EventProxy::~EventProxy()
+{
+    if(m_event)
+        delete m_event;
+}
+
+int EventProxy::register_event(int fd, Handler *handle, EventType type)
+{
+    m_event->register_event(fd, handle, type);
+}
+
+int EventProxy::register_event(SocketInterface::SocketDest &socket, Handler *handle, EventType type)
+{
+    register_event(socket.fd(), handle, type);
+}
+
+int EventProxy::shutdown_event(int fd)
+{
+    m_event->shutdown_event(fd);
+}
+
+int EventProxy::shutdown_event(SocketInterface::SocketDest &socket)
+{
+    m_event->shutdown_event(socket.fd());
+}
+
+} // namespace Event

391fb8a51ad435af358d1dcb16ed280db2d9314a
dzw <dzw0218@163.com>
2019-06-29 21:00:45 +0800

    finish httpserver

--- src/server.cpp
+++ src/server.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "log.h"
+#include "httpserver.h"
 
 int main()
 {
+    HTTPSERVER::HTTPServer httpserver("0.0.0.0", 8080);
+    if(httpserver.start(1024) < 0)
+        return -1;
+
+    while(true)
+    {
+        char buff[1024];
+        char* cmd = fgets(buff, sizeof(buff), stdin);
+        if(cmd == nullptr)
+            break;
+    }
+
     return 0;
 }
